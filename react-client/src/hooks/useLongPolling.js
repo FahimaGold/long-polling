e3f03c21825e43aeb48949c10453b1612e1f2678
Fahima Mokhtari <df_mokhtari@esi.dz>
2025-04-29 16:42:50 +0100

    add django api endpoint

--- react-client/src/hooks/useLongPolling.js
+++ react-client/src/hooks/useLongPolling.js
@@ -3,16 +3,18 @@ import { useState, useEffect, useRef } from 'react';
 export function useLongPolling(clientID) {
     const [messages, setMessages] = useState([]);
     const isMounted = useRef(true);
+    // DjangoAPIURL = http://localhost:8000/poll/
+    // ReactAPIURL = http://localhost:3000/poll
     const doLongPolling = async (clientID) => {
         try {
-          const response = await fetch(`http://localhost:3000/poll/${clientID}`);
+          const response = await fetch(`http://localhost:8000/poll/${clientID}/`);
           if(response.status === 200) {
             const chatMessages = await response.json();
           console.log("Chat messages received:", chatMessages);
            // Ensure the component is still mounted before updating state
            if (isMounted.current) {
             // Update the messages state with the new chat messages
-              setMessages((prev) => [...prev, ...chatMessages]); 
+              setMessages((prev) => [...prev,chatMessages]); 
               // Starting polling again  
               doLongPolling(clientID); 
                   
